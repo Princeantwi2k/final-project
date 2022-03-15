c24458d2ed069f695ebc3d58c7f8056868748621
princeantwi2k <antwi2k@gmail.com>
2022-03-15 14:29:08 +0800

    added store

---
+++ src/Container/App/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { cryptoApi } from "../Body/Welcome";
+import { cryptoNewsApi } from "../Body/CryptoNewsApi";
+
+export default configureStore({
+    reducer:{
+        [cryptoApi.reducerPath]:cryptoApi.reducer,
+        [cryptoNewsApi.reducerPath]:cryptoNewsApi.reducer,
+    }
+}) 
