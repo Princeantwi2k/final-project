6f984b4d102c10cbae841875e7ab0dca7775e43f
princeantwi2k <antwi2k@gmail.com>
2022-03-15 14:33:16 +0800

    added crypto.jsx

---
+++ src/Container/Body/Crypoto.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Chart.css";
+import { Typography } from "antd";
+import { Link } from "react-router-dom";
+import Cryptocurrencies from "./Cryptocurrency";
+
+const { Title } = Typography;
+
+const Crypto = () => {
+  return (
+    <>
+      <div className="chart-body">
+        <div className="crypto-headeing-container">
+          <Title level={2} className="crypto-title">
+            {" "}
+            Top 10 Cryptocurrency
+          </Title>
+          <Title level={3} className="show-more">
+            <Link to="/cryptocurrency">show more</Link>
+          </Title>
+        </div>
+        <Cryptocurrencies simplified />
+      </div>
+    </>
+  );
+};
+
+export default Crypto;
