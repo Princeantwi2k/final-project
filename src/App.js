aeb5be0e65bc5af7fa9e9d17e9b86bdab10b33cf
princeantwi2k <antwi2k@gmail.com>
2022-03-15 14:31:50 +0800

    added App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './Container/Body/Navbar';
+import {Routes, BrowserRouter,Route, Link} from 'react-router-dom'
+import Home from './Container/Pages/Home';
+import Footer from './Container/Body/Footer';
+import News from './Container/Body/News';
+import Cryptocurrencies from './Container/Body/Cryptocurrency';
+import Cryptodetails from './Container/Body/CryptoDetails';
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path="/" exact element={<Home />} />
+      <Route path="/news"  element={<News />} />
+      <Route path="/cryptocurrency"  element={<Cryptocurrencies />} />
+      <Route path="/cryptocurrency:coinId"  element={<Cryptodetails />} />
+    </Routes>
+    </BrowserRouter>
+  
+   
     </div>
   );
 }
