e71a816b50813540e595062e4fe2f3c463475751
princeantwi2k <antwi2k@gmail.com>
2022-03-15 14:36:57 +0800

    added welcome.jsx

---
+++ src/Container/Body/Welcome.jsx
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeader = {
+  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+  "x-rapidapi-key": "375a4c202bmshcfca7e539f18254p1da492jsn3c54507f8b98",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+const createRequest = (url) => ({ url, headers: cryptoApiHeader });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi ",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+  }),
+});
+export const { useGetCryptosQuery } = cryptoApi;
