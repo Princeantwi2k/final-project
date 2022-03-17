a9046dc04ef2af84ff5fa3265e9909c0c408bb1c
princeantwi2k <antwi2k@gmail.com>
2022-03-18 01:43:36 +0800

    added content

--- src/Container/Body/News.jsx
+++ src/Container/Body/News.jsx
@@ -38,8 +38,10 @@ const News = ({ simplified }) => {
             <Option value="Cryptocurrency" className="select-option">
               Cryptocurrency
             </Option>
-            {data?.data?.coins.map((coin) => (
-              <Option value={coin.name}>{coin.name}</Option>
+            {data?.data?.coins.map((coin, i) => (
+              <Option value={coin.name} key={i}>
+                {coin.name}
+              </Option>
             ))}
           </Select>
         </Col>
