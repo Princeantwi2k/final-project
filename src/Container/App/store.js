628a24bf6dfe93d3ab6585ffe71a6780b5b047e6
princeantwi2k <antwi2k@gmail.com>
2022-03-18 01:46:02 +0800

    added a middleware

--- src/Container/App/store.js
+++ src/Container/App/store.js
@@ -3,8 +3,11 @@ import { cryptoApi } from "../Body/Welcome";
 import { cryptoNewsApi } from "../Body/CryptoNewsApi";
 
 export default configureStore({
-    reducer:{
-        [cryptoApi.reducerPath]:cryptoApi.reducer,
-        [cryptoNewsApi.reducerPath]:cryptoNewsApi.reducer,
-    }
-}) 
+    reducer: {
+      [cryptoApi.reducerPath]: cryptoApi.reducer,
+      [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer,
+    },
+    middleware: getDefaultMiddleware =>
+    getDefaultMiddleware().concat(cryptoApi.middleware),
+
+  }); 
