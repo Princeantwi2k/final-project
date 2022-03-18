60ec11350e22f6ff108c8bb34731c783b5fbb03f
princeantwi2k <antwi2k@gmail.com>
2022-03-18 16:58:59 +0800

    added as initials

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import Home from './Container/Pages/Home';
 import News from './Container/Body/News';
 import Cryptocurrencies from './Container/Body/Cryptocurrency';
 import Details from'./Container/Body/Details';
+import Exchanges from './Container/Body/Exchange';
+import Login from './Container/Login/Signup';
+import Signin from './Container/Login/Signin';
+import { UserAuthContextProvider } from './Container/context/AuthContext';
+import ProtectedRoute from './Container/Login/ProtectedRoute';
 
 
 
@@ -15,17 +20,23 @@ import Details from'./Container/Body/Details';
 function App() {
   return (
     <div className="App">
+        <UserAuthContextProvider>
     <BrowserRouter>
-    <Navbar />
+   
+  
     <Routes>
-      <Route path="/" exact element={<Home />} />
-      <Route path="/news" exact  element={<News />} />
-      <Route path="/crypto" exact element={<Cryptocurrencies />} />
-      <Route path='/crypto/:coinId' exact element = {<Details />} />
+      <Route path='/login' exact element={<Login/>} />
+      <Route path='/sign-up' exact element={<Signin/>} />
+      <Route path="/" exact element={<ProtectedRoute><Home /></ProtectedRoute>} />
+      <Route path="/news" exact  element={<ProtectedRoute><News /></ProtectedRoute>} />
+      <Route path="/crypto" exact element={<ProtectedRoute><Cryptocurrencies /> </ProtectedRoute>} />
+      <Route path="/exchange" exact element={<ProtectedRoute><Exchanges/> </ProtectedRoute>} />
+      <Route path='/crypto/:coinId' exact element = {<ProtectedRoute><Details /></ProtectedRoute>} />
     </Routes>
+  
     </BrowserRouter>
   
-   
+    </UserAuthContextProvider>
     </div>
   );
 }
