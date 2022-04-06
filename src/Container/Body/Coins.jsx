7aeed670b6a0dfeacbae0dedb2973f379a97f7b2
princeantwi2k <antwi2k@gmail.com>
2022-04-06 15:28:23 +0800

    added styles

--- src/Container/Body/Coins.jsx
+++ src/Container/Body/Coins.jsx
@@ -18,12 +18,14 @@ const Coins = ({
           <p className="coin-symbol">{symbol}</p>
         </div>
         <div className="coin-data">
-          <p className="coin-price">${price}</p>
-          <p className="coin-volume">${volume}</p>
+          <p className="coin-price"> Price: ${price}</p>
+          <p className="coin-volume"> Volume: ${volume}</p>
           {priceChange < 0 ? (
-            <p className="coin-percent red">{priceChange.toFixed(2)}%</p>
+            <p className="coin-percent red"> Rate: {priceChange.toFixed(2)}%</p>
           ) : (
-            <p className="coin-percent green">{priceChange.toFixed(2)}%</p>
+            <p className="coin-percent green">
+              Rate: {priceChange.toFixed(2)}%
+            </p>
           )}
           <p className="coin-marketcap"> Mkt cap: ${marketcap}</p>
         </div>
